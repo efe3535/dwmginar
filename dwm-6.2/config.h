b4dc13ba49c42170d1ec66389249a84e2d4ebaab
efe3535 <a.efe.akyazi@gmail.com>
2021-12-15 19:37:29 +0300

    added st, with few changes to luke's setting.

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -14,7 +14,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 // static const char *fonts[]          = { "Jetbrains Mono:size=8" };
 // static const char *fonts[]	    = {"Fira Code Nerd Font:size=9"};
-static const char *fonts[]		=	{ "JetBrainsMonoMedium Nerd Font:size=9" };
+static const char *fonts[]		=	{ "Hack Nerd Font:size=9" };
 // static const char *fonts[]			=	{ "JetBrainsMonoMedium Nerd Font:size=10" };
 static const char dmenufont[]       = "Iosevka Nerd Font:size=12";
 static const char col_gray1[]       = "#00080B"; // prev #00020e    
@@ -78,8 +78,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //static const char *roficmd[] = { "rofi","-show-icons","-modi drun,run","-font 'Jetbrains Mono 12'", "-show drun", NULL };
-static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons","-font 'Poppins 12'", NULL };
-static const char *termcmd[]  = { "xfce4-terminal", NULL };
+static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons","-font 'scientifica Nerd Font 12'", NULL };
+static const char *termcmd[]  = { "st", "zsh", NULL };
 static const char *upvol[]   = { "amixer", "set", "Master", "5%+",     NULL };
 static const char *downvol[] = { "amixer", "set", "Master", "5%-",     NULL };
 static const char *mutevol[] = { "amixer", "set", "Master", "toggle",  NULL };
